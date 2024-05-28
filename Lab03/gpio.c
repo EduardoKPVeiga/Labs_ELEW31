818aee2c4daa563011895edf3701e28ab912bc5e
Fernando Abreu <fabreu.real@gmail.com>
2024-05-28 00:44:52 -0300

    Control Leds for putty

--- Lab03/gpio.c
+++ Lab03/gpio.c
@@ -7,13 +7,21 @@
 #include <stdint.h>
 
 #include "tm4c1294ncpdt.h"
-  
-#define GPIO_PORTA  (0x0001) //bit 1
+ // QPNMLKJHGFEDCBA
+ // EDCBA9876543210
+#define GPIO_PORTA  (0x0001) //bit 0
 #define GPIO_PORTJ  (0x0100) //bit 8
 #define GPIO_PORTN  (0x1000) //bit 12
 
 void SysTick_Wait1ms(uint32_t delay);
-
+void GPIO_Init(void);
+uint32_t PortJ_Input(void);
+void PortN_Output(uint32_t valor);
+void WriteTX(char value);
+uint8_t ReadRX(void);
+void clean_putty(void);
+void move_cursor_beginning_putty(void);
+void Write_line_putty(char* line);
 // -------------------------------------------------------------------------------
 // Função GPIO_Init
 // Inicializa os ports J e N
@@ -24,7 +32,7 @@ void GPIO_Init(void)
 	//1a. Ativar o clock para a porta setando o bit correspondente no registrador RCGCGPIO
 	SYSCTL_RCGCGPIO_R = (GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA);
 	//1b.   após isso verificar no PRGPIO se a porta está pronta para uso.
-  while((SYSCTL_PRGPIO_R & (GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA) ) != (GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA) ){};
+  while((SYSCTL_PRGPIO_R & (GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA) ) != (GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA) ){}
 	
 	// 2. Limpar o AMSEL para desabilitar a analógica
 	GPIO_PORTA_AHB_AMSEL_R = 0x00;
@@ -32,10 +40,12 @@ void GPIO_Init(void)
 	GPIO_PORTN_AMSEL_R = 0x00;
 		
 	// 3. Limpar PCTL para selecionar o GPIO
+	GPIO_PORTA_AHB_PCTL_R = 0x11;
 	GPIO_PORTJ_AHB_PCTL_R = 0x00;
 	GPIO_PORTN_PCTL_R = 0x00;
 
 	// 4. DIR para 0 se for entrada, 1 se for saída
+	GPIO_PORTA_AHB_DIR_R = 0x00;
 	GPIO_PORTJ_AHB_DIR_R = 0x00;
 	GPIO_PORTN_DIR_R = 0x03; //BIT0 | BIT1
 		
@@ -45,7 +55,7 @@ void GPIO_Init(void)
 	GPIO_PORTN_AFSEL_R = 0x00;
 		
 	// 6. Setar os bits de DEN para habilitar I/O digital	
-	GPIO_PORTA_AHB_DEN_R = 0x02;   //Bit0 e bit1
+	GPIO_PORTA_AHB_DEN_R = 0x03;   //Bit0 = entrada e bit1 = saida
 	GPIO_PORTJ_AHB_DEN_R = 0x03;   //Bit0 e bit1
 	GPIO_PORTN_DEN_R = 0x03; 		   //Bit0 e bit1
 	
@@ -56,25 +66,48 @@ void GPIO_Init(void)
 	GPIO_PORTA_AHB_PCTL_R = 0x11;
 	
 	// Uart run mode clock gating control
-	SYSCTL_RCGCUART_R = SYSCTL_RCGCUART_R0;
+	SYSCTL_RCGCUART_R = SYSCTL_RCGCUART_R0;			// ativa o bit 00000001
 	while (SYSCTL_PRUART_R != SYSCTL_PRUART_R0)
 		SysTick_Wait1ms(100);
 	
 	// Uart control
-	UART0_CTL_R = 0x00;
+	// RXE(habilita o receptor) = bit9 / TXE(habilita o transmissor) = bit8 / UARTEN(habilita a UART) = bit0
+	UART0_CTL_R = 0x00;												// desativando a UART
 	
 	// Uart Baud Rate
-	UART0_IBRD_R = 260;
-	UART0_FBRD_R = 27;
+	// Baud Rate 19200 bits/seg
+	// BRD = 80M / (16 * BaudRate) = 260,4167
+	UART0_IBRD_R = 260;											 	// 16 bits para parte inteira 
+	UART0_FBRD_R = 27;												// 6 bits para parte fracionada 0,4167 * 64 = 26,67 = 27
 	
 	// Num. bits, paridade, stop bits
-	UART0_LCRH_R = 0x78;
+	/*
+	 * bit 6 e bit5 = WLEN: define o tamanho da palavra sendo 5 + bit6bit5(5 a 8 bits)
+	 * bit 4 = FEN: habilita a fila
+	 * bit 3 = bits de stop
+	 * 		0 => 1 bit de stop
+	 * 		1 => 2 bits de stop
+	 * bit 2
+	 *		0 => paridade ímpar
+	 *		1 => paridade par
+	 * bit 1 = PEN(Parity Enable) : habilita a paridade
+   *		0 => desabilita
+	 *		1 => habilita
+	 * bit 0 = BRK
+	 *		0 => normal uso
+	 *		1 => nao sei
+	 */
+	UART0_LCRH_R = 0x78;		// 0111 1000
+	// Start(1) + Word(8 bits) + Paridade(0) + Stop(2) = 11 bits
 	
 	// Select clock source
 	UART0_CC_R = 0x00;
 	
 	// Uart control
-	UART0_CTL_R = UART0_CTL_R | 0x301;
+	UART0_CTL_R = UART0_CTL_R | 0x301; // setando os bits 0011 0000 0001 habilitando TX, RX, UARTEN(uart enable)
+	
+	GPIO_PORTA_AHB_PCTL_R = 0x11;			// 0001 0001 A1 e A0 recebe a função 1
+	GPIO_PORTA_AHB_AFSEL_R = 0x03;		// 0000 0011 habilita os pinos A1 e A0 para função alternativas
 }
 
 // -------------------------------------------------------------------------------
@@ -97,25 +130,48 @@ void PortN_Output(uint32_t valor)
     uint32_t temp;
     //vamos zerar somente os bits menos significativos
     //para uma escrita amigável nos bits 0 e 1
-    temp = GPIO_PORTN_DATA_R & 0xFC;
+    temp = GPIO_PORTN_DATA_R & 0xFC; // 1111 1100
     //agora vamos fazer o OR com o valor recebido na função
     temp = temp | valor;
     GPIO_PORTN_DATA_R = temp; 
 }
 
-uint8_t ReadRX()
+uint8_t ReadRX(void)
 {
-	while ((UART0_FR_R & 0x10) == 0x10)
-		SysTick_Wait1ms(1);
+	// bit 4 => RXFE indica que não é possivel a leitura
+	while ((UART0_FR_R & 0x10) == 0x10){}	// 0001 0000 => verifica se é possivel a leitura
+		//SysTick_Wait1ms(1);
 	uint8_t value = (uint8_t)UART0_DR_R;
 	return value;
 }
 
-void WriteTX(uint8_t value)
+// inicia a transmissão de dados
+void WriteTX(char value)
 {
-	while ((UART0_FR_R & 0x20) == 0x20)
-		SysTick_Wait1ms(1);
-	UART0_DR_R = value;
+	// bit 5 => TXFF indica que não é possivel a escrita
+	while ((UART0_FR_R & 0x20) == 0x20){} // 0010 0000 => verifica se é possivel a escrita
+		//SysTick_Wait1ms(1);
+	UART0_DR_R = (char)value;
 }
 
+void clean_putty(void) {
+	WriteTX(0x1B);
+	WriteTX('[');
+	WriteTX('2');
+	WriteTX('J');
+}
 
+void move_cursor_beginning_putty(void) {
+	WriteTX(0x1B);
+	WriteTX('[');
+	WriteTX(';');
+	WriteTX('H');
+}
+
+void Write_line_putty(char* line) {
+	while(*line != '\0') {
+		WriteTX(*line);
+		line++;
+	}
+	//WriteTX('\n');
+}
--- Lab03/main.c
+++ Lab03/main.c
@@ -14,20 +14,47 @@ void GPIO_Init(void);
 uint32_t PortJ_Input(void);
 void PortN_Output(uint32_t leds);
 void Pisca_leds(void);
-void WriteTX(uint8_t value);
+void WriteTX(char value);
 uint8_t ReadRX(void);
+void acender_ledsN(uint8_t vn);
+void clean_putty(void);
+void move_cursor_beginning_putty(void);
+void Write_line_putty(char* line);
 
 int main(void)
 {
 	PLL_Init();
 	SysTick_Init();
 	GPIO_Init();
+	uint8_t var = 0x00;
 	while (1)
 	{
-		uint8_t var = ReadRX();
-		SysTick_Wait1ms(1000);
-    WriteTX(var);
+		Write_line_putty("Digite um comando:");
+		var = ReadRX();
+		clean_putty();
+		move_cursor_beginning_putty();
+		//SysTick_Wait1ms(1000);
+		switch(var) {
+			case '0':
+				acender_ledsN(0x00);
+				break;
+			case 	'1':
+				acender_ledsN(0x01); // acende uma led
+				break;
+			case	't':
+				acender_ledsN(0x03);	// acende todas as leds
+				break;
+			default:
+				break;
+		}
+		//acender_ledsN(0x03);
+		//SysTick_Wait1ms(3000);
+    //WriteTX(var);
 	}
+}
+
+void acender_ledsN(uint8_t vn) {
+	PortN_Output(vn);
 }
 
 void Pisca_leds(void)
